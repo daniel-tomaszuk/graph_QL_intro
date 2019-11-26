3277f35bfdc800ce38b91d53dcdd2ec24a896e14
Daniel Tomaszuk <daniel.tomaszuk@netguru.pl>
2019-11-26 12:53:52 +0100

    Allow ALL CORS.

--- f2f_Q3_2019/f2f_Q3_2019/settings.py
+++ f2f_Q3_2019/f2f_Q3_2019/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'graphene_django',
     'core',
@@ -59,12 +60,15 @@ GRAPHENE = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'f2f_Q3_2019.urls'
 
--- f2f_Q3_2019/tracker/views.py
+++ f2f_Q3_2019/tracker/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
@@ -7,6 +8,7 @@ class MapGraphQLTemplateView(LoginRequiredMixin, TemplateView):
     Servers map template for GraphQL API.
     """
     template_name = 'tracker/map_graphql_template.html'
+    login_url = reverse_lazy('admin:login')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,6 +20,7 @@ class MapRESTTemplateView(LoginRequiredMixin, TemplateView):
     Serves map template for REST API.
     """
     template_name = 'tracker/map_rest_template.html'
+    login_url = reverse_lazy('admin:login')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
