0d36f5d7563995bea370ac19be46aa53cf531677
dghy <dtomaszuk01@gmail.com>
2019-12-16 18:39:11 +0100

    Ling vue.js with django.

---
+++ f2f_Q3_2019/core/static/core/components/make-request.js
@@ -0,0 +1,22 @@
+
+
+function makeRequest(method, url, positionQuery) {
+    const response = ''
+    var urlParams = ''
+    if (positionQuery) {
+        url = url + '?query=' + positionQuery
+    }
+    return axios({
+        method: method,
+        url: url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(function (response) {
+        console.log('Successful data fetch.')
+        return response.data
+      }).catch(function (error) {
+        console.log('Error has occured: ' + error.statusTet)
+        console.log(error.data)
+      })
+}
