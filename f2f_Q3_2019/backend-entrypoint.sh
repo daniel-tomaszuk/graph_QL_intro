28a6dc08f336e2e374f1c6a25270a356249693c4
dghy <dtomaszuk01@gmail.com>
2019-12-29 10:30:44 +0100

    Use gunicorn as wsgi server.

--- f2f_Q3_2019/backend-entrypoint.sh
+++ f2f_Q3_2019/backend-entrypoint.sh
@@ -2,4 +2,4 @@
 python wait_for_db.py
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py runserver 0.0.0.0:8000
+gunicorn -w 5 --bind 0.0.0.0:8000 --forwarded-allow-ips='*' f2f_Q3_2019.wsgi --log-level DEBUG
