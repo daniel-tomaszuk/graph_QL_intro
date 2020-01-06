ff6013d133d6cfb11317a400b00976ac0bf15120
dghy <dtomaszuk01@gmail.com>
2020-01-06 10:53:07 +0100

    Use Nginx as static files handler. S3 free has been used for this month.

--- f2f_Q3_2019/f2f_Q3_2019/settings.py
+++ f2f_Q3_2019/f2f_Q3_2019/settings.py
@@ -166,18 +166,17 @@ AWS_DEFAULT_ACL = "public-read"
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",  # 24h
 }
 
 # Static files
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, "static")
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # N2YO Config
 N2YO_BASE_URL_V1 = "https://www.n2yo.com/rest/v1/satellite/"
