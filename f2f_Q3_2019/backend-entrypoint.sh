32cfc964fb0f31582fbdd1936a9b2705b56d57b8
dghy <dtomaszuk01@gmail.com>
2020-01-16 11:58:31 +0100

    Set gunicorn to DEBUG.

--- f2f_Q3_2019/backend-entrypoint.sh
+++ f2f_Q3_2019/backend-entrypoint.sh
@@ -2,4 +2,4 @@
 python wait_for_db.py
 python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn -c './gunicorn.conf.py' f2f_Q3_2019.wsgi --log-level INFO
+gunicorn -c './gunicorn.conf.py' f2f_Q3_2019.wsgi --log-level DEBUG
