53b442eab20ae413f916432a8fed3f6c6135a543
dghy <dtomaszuk01@gmail.com>
2020-01-27 07:59:21 +0100

    Set Admin to read only.

--- f2f_Q3_2019/tracker/admin.py
+++ f2f_Q3_2019/tracker/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from tracker.models import Position, Satellite
+from tracker.models import Position
+from tracker.models import Satellite
 
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    pass
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Satellite)
 class SatelliteAdmin(admin.ModelAdmin):
-    pass
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
